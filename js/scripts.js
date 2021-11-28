4ac38a53f1ec99c5a7b0e9118177e64fe15db302
Haziq Roslan <mohd.haziq.roslan@gmail.com>
2021-11-29 00:33:38 +0800

    Fix: Youtube video

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxSZh0zzyEApGi5H8r5WVxzq9W6zgvGpPq6alY_Y-6zrpiLwv0V25GzxpXdEXQzwYCH/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyb3uGE6mE8hkf3_7QmUdDSZxHiwfujhqLwATM_5S8LSoyX8gcujPRDYoZMX93sAK9E/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -236,6 +236,33 @@ $(document).ready(function () {
     });
 
 });
+
+    /********************** RSVP Exp **********************/
+    $('#rsvp-form-2').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+
+        if (MD5($('#invite_code').val()) !== '6dd84f5f9ea3f610dbcf08b1758be6b9') {
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        } else {
+            $.post('https://script.google.com/macros/s/AKfycbyb3uGE6mE8hkf3_7QmUdDSZxHiwfujhqLwATM_5S8LSoyX8gcujPRDYoZMX93sAK9E/exec', data)
+                .done(function (data) {
+                    console.log(data);
+                    if (data.result === "error") {
+                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+                    } else {
+                        $('#alert-wrapper').html('');
+                        $('#rsvp-modal').modal('show');
+                    }
+                })
+                .fail(function (data) {
+                    console.log(data);
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                });
+        }
+    });
 
 /********************** Extras **********************/
 
